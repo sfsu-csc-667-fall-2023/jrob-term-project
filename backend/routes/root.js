f2186edc6b3ecd56b1eb650dbda487be86952a1d
John Roberts <roberts.john@gmail.com>
2023-10-19 19:38:14 -0700

    Set up fe/be, basic server with a route

---
+++ backend/routes/root.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get("/", (_request, response) => {
+  response.send("Hello world from inside a route!");
+});
+
+router.get("/:id", (request, response) => {
+  response.send(`Other route ${request.params.id}`);
+})
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const rootRoutes = require("./routes/root");
+
+app.use("/", rootRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
