12692c3cb8a318dbd6d8d0a2485794cd845873a2
John Roberts <roberts.john@gmail.com>
2023-10-19 20:47:21 -0700

    some added server functionality

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,34 @@ const path = require("path");
 
 const express = require('express');
 const createError = require("http-errors");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "static")));
 
 const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === "development") {
+  const livereload = require("livereload");
+  const connectLiveReload = require("connect-livereload");
+
+  const liveReloadServer = livereload.createServer();
+  liveReloadServer.watch(path.join(__dirname, "static"));
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+  });
+
+  app.use(connectLiveReload());
+}
 
 const rootRoutes = require("./routes/root");
 app.use("/", rootRoutes);
