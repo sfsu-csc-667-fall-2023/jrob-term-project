40eabda7fc3cbd7f5c99ea04174165b774373a53
John Roberts <roberts.john@gmail.com>
2023-11-13 20:35:04 -0800

    added sign up/in forms

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,16 @@ const express = require("express");
 const createError = require("http-errors");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
